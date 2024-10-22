5dbcea16334f5bfa702b90a31caf61ee6d641ed6
Hubert Janoszka <HubertPNJ@protonmail.com>
2024-10-23 00:25:28 +0200

    Initial commit

---
+++ src/main/java/hpnj/mediatrack/MediatrackApplication.java
@@ -0,0 +1,13 @@
+package hpnj.mediatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediatrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediatrackApplication.class, args);
+    }
+
+}
