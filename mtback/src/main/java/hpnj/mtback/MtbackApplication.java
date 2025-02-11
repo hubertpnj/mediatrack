cf3e0669c149c0e2a3236f8ed120becd1f6f3704
Hubert Janoszka <HubertPNJ@protonmail.com>
2025-02-11 18:55:27 +0100

    Backend & frontend generated

---
+++ mtback/src/main/java/hpnj/mtback/MtbackApplication.java
@@ -0,0 +1,13 @@
+package hpnj.mtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtbackApplication.class, args);
+	}
+
+}
---
+++ mtback/src/test/java/hpnj/mtback/MtbackApplicationTests.java
@@ -0,0 +1,13 @@
+package hpnj.mtback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
