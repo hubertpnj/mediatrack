317896fe80a627e3c841bff31c394b5f438f7e4d
Hubert Janoszka <HubertPNJ@protonmail.com>
2026-05-14 21:42:27 +0200

    Add PasswordEncoderConfig and UserDetailsService

---
+++ backend/src/main/java/hpnj/mediatrack/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package hpnj.mediatrack.service;
+
+import hpnj.mediatrack.repository.UserAccountRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserAccountRepository repository;
+
+    public UserDetailsServiceImpl(UserAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with email: " + email));
+    }
+}
